7ea7ae24a9f60524d203eeadc2cb21d068338e36
ofertaramadani <oferte.ramadani@student.uni-pr.edu>
2022-05-15 20:07:30 +0200

    Update DES_ALGORITHM.cpp

--- DES_ALGORITHM/DES_ALGORITHM/DES_ALGORITHM.cpp
+++ DES_ALGORITHM/DES_ALGORITHM/DES_ALGORITHM.cpp
@@ -76,3 +76,75 @@ string Xor(string a, string b) {
 	}
 	return result;
 }
+void generate_keys(string key) {
+	// Tabela PC1
+	int pc1[56] = {
+	57,49,41,33,25,17,9,
+	1,58,50,42,34,26,18,
+	10,2,59,51,43,35,27,
+	19,11,3,60,52,44,36,
+	63,55,47,39,31,23,15,
+	7,62,54,46,38,30,22,
+	14,6,61,53,45,37,29,
+	21,13,5,28,20,12,4
+	};
+	// Tabela PC2
+	int pc2[48] = {
+	14,17,11,24,1,5,
+	3,28,15,6,21,10,
+	23,19,12,4,26,8,
+	16,7,27,20,13,2,
+	41,52,31,37,47,55,
+	30,40,51,45,33,48,
+	44,49,39,56,34,53,
+	46,42,50,36,29,32
+	};
+	string perm_key = "";
+	for (int i = 0; i < 56; i++) {
+		perm_key += key[pc1[i] - 1];
+	}
+	string left = perm_key.substr(0, 28);
+	string right = perm_key.substr(28, 28);
+	for (int i = 0; i < 16; i++) {
+		if (i == 0 || i == 1 || i == 8 || i == 15) {
+			left = shift_left_once(left);
+			right = shift_left_once(right);
+		}
+		else {
+			left = shift_left_twice(left);
+			right = shift_left_twice(right);
+		}
+		string combined_key = left + right;
+		string round_key = "";
+		for (int i = 0; i < 48; i++) {
+			round_key += combined_key[pc2[i] - 1];
+		}
+		round_keys[i] = round_key;
+
+		cout << "Key " << i + 1 << ": " << round_keys[i] << endl;
+
+	}
+
+}
+
+string DES() {
+
+	int initial_permutation[64] = {
+	58,50,42,34,26,18,10,2,
+	60,52,44,36,28,20,12,4,
+	62,54,46,38,30,22,14,6,
+	64,56,48,40,32,24,16,8,
+	57,49,41,33,25,17,9,1,
+	59,51,43,35,27,19,11,3,
+	61,53,45,37,29,21,13,5,
+	63,55,47,39,31,23,15,7
+	};
+
+	int expansion_table[48] = {
+	32,1,2,3,4,5,4,5,
+	6,7,8,9,8,9,10,11,
+	12,13,12,13,14,15,16,17,
+	16,17,18,19,20,21,20,21,
+	22,23,24,25,24,25,26,27,
+	28,29,28,29,30,31,32,1
+	};
