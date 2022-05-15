9752b29bc51f03027ec4171e725aca51708e9859
olsajupolli <94190695+olsajupolli@users.noreply.github.com>
2022-05-15 20:00:45 +0200

    Des algorithm

---
+++ DES_ALGORITHM/DES_ALGORITHM/DES_ALGORITHM.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <bitset>
+using namespace std;
+string round_keys[16];
+string pt;
+
+string TextToBinaryString(string words) {
+	string binaryString = "";
+	for (char& _char : words) {
+		binaryString += bitset<8>(_char).to_string();
+	}
+	return binaryString;
+}
+
+string convertDecimalToBinary(int decimal)
+{
+	string binary;
+	while (decimal != 0) {
+		binary = (decimal % 2 == 0 ? "0" : "1") + binary;
+		decimal = decimal / 2;
+	}
+	while (binary.length() < 4) {
+		binary = "0" + binary;
+	}
+	return binary;
+}
+
+int convertBinaryToDecimal(string binary)
+{
+	int decimal = 0;
+	int counter = 0;
+	int size = binary.length();
+	for (int i = size - 1; i >= 0; i--)
+	{
+		if (binary[i] == '1') {
+			decimal += pow(2, counter);
+		}
+		counter++;
+	}
+	return decimal;
+}
+
+string shift_left_once(string key_chunk) {
+	string shifted = "";
+	for (int i = 1; i < 28; i++) {
+		shifted += key_chunk[i];
+	}
+	shifted += key_chunk[0];
+	return shifted;
+}
+string shift_left_twice(string key_chunk) {
+	string shifted = "";
+	for (int i = 0; i < 2; i++) {
+		for (int j = 1; j < 28; j++) {
+			shifted += key_chunk[j];
+		}
+		shifted += key_chunk[0];
+		key_chunk = shifted;
+		shifted = "";
+	}
+	return key_chunk;
+}
+
+string Xor(string a, string b) {
+	string result = "";
+	int size = b.size();
+	for (int i = 0; i < size; i++) {
+		if (a[i] != b[i]) {
+			result += "1";
+		}
+		else {
+			result += "0";
+		}
+	}
+	return result;
+}
